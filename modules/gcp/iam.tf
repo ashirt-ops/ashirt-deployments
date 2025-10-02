90bcc7c50f794451a0ffa96f81812f3ce4367030
Joe Rozner <joe@deadbytes.net>
2025-10-02 15:25:30 -0700

    Fix for terraform

--- modules/gcp/iam.tf
+++ modules/gcp/iam.tf
@@ -1,23 +1,23 @@
 resource "google_project_iam_member" "backend-storage-get" {
   project = var.project
   role    = "roles/storage.objects.get"
-  member  = google_service_account.backend.name
+  member  = google_service_account.backend
 }
 
 resource "google_project_iam_member" "backend-storage-list" {
   project = var.project
   role    = "roles/storage.objects.list"
-  member  = google_service_account.backend.name
+  member  = google_service_account.backend
 }
 
 resource "google_project_iam_member" "backend-storage-create" {
   project = var.project
   role    = "roles/storage.objects.create"
-  member  = google_service_account.backend.name
+  member  = google_service_account.backend
 }
 
 resource "google_project_iam_member" "backend-storage-delete" {
   project = var.project
   role    = "roles/storage.objects.delete"
-  member  = google_service_account.backend.name
+  member  = google_service_account.backend
 }
